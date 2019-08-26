2fc77baf4e6e9c75bca26173f6fcdf9f8b2b72ca
Sami Hamici <sami.hamici@corefactory.com>
2019-08-26 14:03:31 +0300

    Aded viewHelper to split code for unit tests

---
+++ cartracker/rest/viewHelper.py
@@ -0,0 +1,37 @@
+
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import permission_classes, authentication_classes
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate
+
+from rest_framework.authtoken.models import Token
+
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
+
+# Create new user if user doesn't exist
+@permission_classes((AllowAny,))
+def createUser(username, password):
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'},
+                status = HTTP_400_BAD_REQUEST)
+
+    user = authenticate(username = username, password = password)
+    if not user:
+        user = User.objects.create_user(username=username, password = password)
+        #Create userstatus 
+        userstatus = UserStatus.objects.create()
+        userstatus.username = username
+        userstatus.location = "unknown"
+        userstatus.save()
+    token, _= Token.objects.get_or_create(user = user)
+    
+    return Response({'token': token.key}, status = HTTP_200_OK)
--- cartracker/rest/views.py
+++ cartracker/rest/views.py
@@ -4,16 +4,19 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User, Group
 
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
-
+from django.contrib.auth import authenticate
 from .models import UserStatus, GPSCoordinates
+from .viewHelper import createUser
 
 from rest_framework.status import (
     HTTP_400_BAD_REQUEST,
@@ -27,22 +30,8 @@ from rest_framework.status import (
 def CreateUserView(request):
     username = request.data.get("username")
     password = request.data.get("password")
-
-    if username is None or password is None:
-        return Response({'error': 'Please provide both username and password'},
-                status = HTTP_400_BAD_REQUEST)
-
-    user = authenticate(username = username, password = password)
-    if not user:
-        user = User.objects.create_user(username=username, password = password)
-        #Create userstatus 
-        userstatus = UserStatus.objects.create()
-        userstatus.username = username
-        userstatus.location = "unknown"
-        userstatus.save()
-    token, _= Token.objects.get_or_create(user = user)
-    
-    return Response({'token': token.key}, status = HTTP_200_OK)
+    createUser(username, password)
+    return createUser(username, password)
 
 @csrf_exempt
 @api_view(["POST"])
